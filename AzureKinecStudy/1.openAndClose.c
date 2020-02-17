4841b1fa81131354ac11d9841a8d40168f374e4a
Wangxy2180 <50308058+Wangxy2180@users.noreply.github.com>
2020-02-17 13:56:05 +0800

    Update 1.openAndClose.c

--- AzureKinecStudy/1.openAndClose.c
+++ AzureKinecStudy/1.openAndClose.c
@@ -13,11 +13,15 @@ int main()
 		printf("AzureKinect尚未连接至设备\n");
 		return;
 	}
-	else printf("AzureKInect设备数为：%d", Kinect_count);
+	else printf("AzureKInect设备数为：%d\n", Kinect_count);
 
 	//打开Kinect设备
 	k4a_device_t Kinect_1 = NULL;
-	k4a_device_open(K4A_DEVICE_DEFAULT, Kinect_1);
+	if (K4A_FAILED(k4a_device_open(K4A_DEVICE_DEFAULT, &Kinect_1)))
+	{
+		printf("AzureKinect打开失败\n");
+		return;
+	}
 
 	//获取序列号，确保能用
 	//先获取序列号size，再申请空间
@@ -35,11 +39,16 @@ int main()
 	Kinect_1_config.camera_fps		 = K4A_FRAMES_PER_SECOND_30;
 
 	//根据给定配置打开箱机
-	k4a_device_start_cameras(Kinect_1, &Kinect_1_config);
+	if (K4A_FAILED(k4a_device_start_cameras(Kinect_1, &Kinect_1_config)))
+	{
+		printf("打开相机失败!\n");
+		return;
+	}
 
 	//这其中开始对其进行各种处理
 
 	//关闭摄像机和设备
 	k4a_device_stop_cameras(Kinect_1);
 	k4a_device_close(Kinect_1);
+	return 0;
 }
